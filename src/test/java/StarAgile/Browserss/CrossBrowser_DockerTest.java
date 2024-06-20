070cb0607c37c08db4ed39477960e0999a8c674c
Devil <Devil@DESKTOP-ST8EDP8.mshome.net>
2024-06-20 11:42:37 +0530

    DevopsAssignment

---
+++ src/test/java/StarAgile/Browserss/CrossBrowser_DockerTest.java
@@ -0,0 +1,66 @@
+package StarAgile.Browserss;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CrossBrowser_DockerTest {
+    WebDriver driver;
+	@Parameters({"bname"})
+	@Test
+	public void browserTest(String bname) throws MalformedURLException, InterruptedException {
+		if(bname.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			driver= new RemoteWebDriver(new URL("http://localhost:4444"),options);
+			System.out.println("Connection established with chrome browser");
+			}
+		if(bname.equalsIgnoreCase("Firefox")) { 
+			FirefoxOptions options = new FirefoxOptions();
+			driver= new RemoteWebDriver(new URL("http://localhost:4444"),options);
+			System.out.println("Connection established with Firefox browser");
+			}
+		if(bname.equalsIgnoreCase("Edge")) { 
+			EdgeOptions options = new EdgeOptions();
+			driver= new RemoteWebDriver(new URL("http://localhost:4444"),options);
+			System.out.println("Connection established with Edge browser");
+			}
+		Thread.sleep(5000);
+		driver.get("https://www.amazon.in/");
+		Thread.sleep(3000);
+		System.out.println("Application executing paralelly");
+		driver.quit();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
